8050152bb8ceb9913a0379cf0b29cacd5c329e1a
nickelite93 <nicholas.scott0601@gmail.com>
2022-03-18 12:21:42 +0000

    First commit

---
+++ src/main/java/com/project/recipeprovider/RecipeproviderApplication.java
@@ -0,0 +1,13 @@
+package com.project.recipeprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeproviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/recipeprovider/RecipeproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.recipeprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
